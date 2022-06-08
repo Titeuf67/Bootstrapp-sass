395e626cfd2adc00905941fe2a9420f90d8fcf21
Titeuf67 <christian.fillette@gmail.com>
2022-06-08 15:34:49 +0200

    08/06

---
+++ assets/js/index.js
@@ -0,0 +1,6 @@
+// const text = prompt('Entrez votre nom ?')
+// const writeName = document.getElementById("name")
+// writeName.textContent = "Welcome " + text
+
+// console.log("Je suis un log", document)
+// console.log("Je suis un log",text, writeName)
